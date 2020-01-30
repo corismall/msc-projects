5a4e1aa92e197a1f81ec15e06acfd6ec98e671e5
c_desktop <corinn.small@gmail.com>
2020-01-30 15:48:43 +0100

    Update high_impact_genes.py

--- bio608/high_impact_genes.py
+++ bio608/high_impact_genes.py
@@ -1,19 +1,12 @@
-#for counting number of high impact genes above a given threshold
+'''Counts number of high impact genes above a given threshold/filters matched
+ SnpEff genes that contain 10+ SNPs'''
 import pandas as pd
-import csv 
-import numpy as np 
+import csv
+import numpy as np
 
 data=pd.read_csv('/home/small/Documents/Biol608_project/ComparisonTempGenes_Snps.out', delimiter= '\t', sep=',')
 df=pd.DataFrame(data)
 #print data
 datafil=(data.loc[data['Variants impact HIGH'] >= 10])
 print datafil
-print len(datafil) 
-
-
-
-
-
-
-
-
+print len(datafil)
